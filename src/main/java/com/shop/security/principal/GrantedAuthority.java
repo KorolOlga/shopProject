70769c1b6b489c6db4b2958bbf567d754d17ecef
Ольга Король < >
2016-04-13 10:45:28 +0300

    Initial commit

---
+++ src/main/java/com/shop/security/principal/GrantedAuthority.java
@@ -0,0 +1,7 @@
+package com.shop.security.principal;
+
+/**
+ * Created by Olya on 13.04.2016.
+ */
+public interface GrantedAuthority {
+}
---
+++ src/test/java/com/shop/LogicTest.java
@@ -0,0 +1,13 @@
+package com.shop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LogicTest {
+    @Test
+    public void logicTest(){
+        Assert.assertEquals(1,1);
+        Assert.assertEquals(2+2*2,6);
+
+    }
+}
